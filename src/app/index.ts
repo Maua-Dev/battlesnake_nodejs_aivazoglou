6179396d3dae5bf856e74ebb8d5a1cefb9cddcc3
Gustavo Aivazoglou <gusaivazoglou@gmail.com>
2024-07-31 02:43:10 -0300

    não contar com minha tail

--- src/app/index.ts
+++ src/app/index.ts
@@ -91,6 +91,14 @@ app.post('/move', (req: Request, res: Response) => {
     }
 
     posicoes_ocupadas.push(...limites);
+    let index_tail = posicoes_ocupadas.indexOf(tail);
+
+    // Verificar se o valor existe no array
+    if (index_tail !== -1) {
+        // Remover o valor do array
+        posicoes_ocupadas.splice(index_tail, 1);
+    }
+
     const comidas = [...req.body.board.food];
 
     const head = req.body.you.head;
