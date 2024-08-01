f7df6488537433ee12614d9e155466b32e538e66
Gustavo Aivazoglou <gusaivazoglou@gmail.com>
2024-07-31 23:46:42 -0300

    teste

--- src/app/index.ts
+++ src/app/index.ts
@@ -237,8 +237,8 @@ app.post('/move', (req: Request, res: Response) => {
     }
 
     const response = {
-        //move: finalDirection || "up",
-        move: "up",
+        move: finalDirection || "up",
+        //move: "up",
         shout: 'Moving towards food!'
     };
     res.json(response);
